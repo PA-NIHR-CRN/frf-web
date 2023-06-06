d73b110e97b377f94b88bd1d410c2dbb62effe5b
tctc91 <git-commit-address>
2023-06-06 12:25:22 +0100

    refactor: footer responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 export function Footer() {
   return (
     <footer className={clsx('govuk-footer', 'py-0')} role="contentinfo">
-      <div className={clsx('bg-navy-100 text-white')} data-testid="frf-footer-links">
+      <div className="bg-navy-100 text-white" data-testid="frf-footer-links">
         <div className="govuk-width-container">
-          <div className={clsx('flex h-[var(--footer-links-panel-height)] items-center justify-end')}>
+          <div className="flex h-[var(--footer-links-panel-height)] items-center justify-end">
             <h2 className="govuk-visually-hidden">Support links</h2>
             <ul className="govuk-footer__inline-list mb-0">
               <li className="govuk-footer__inline-list-item">
@@ -27,47 +27,43 @@ export function Footer() {
       </div>
       <div className={clsx('bg-white')} data-testid="frf-footer-logos">
         <div className="govuk-width-container">
-          <div className="flex items-center justify-between gap-12 md:h-[var(--footer-logos-panel-height)]">
-            <div className="flex items-center">
-              <a href="#" target="_blank">
-                <Image
-                  src="/assets/logos/nihr.svg"
-                  width={240}
-                  height={24}
-                  alt="National Institute for Health and Care Research logo (opens in a new window)"
-                />
-              </a>
-            </div>
-            <div className="flex items-center">
-              <a href="#" target="_blank">
-                <Image
-                  src="/assets/logos/hsc-public-health-agency.svg"
-                  width={253}
-                  height={97}
-                  alt="HSC Public Health Agency logo (opens in a new window)"
-                />
-              </a>
-            </div>
-            <div className="flex items-center">
-              <a href="#" target="_blank">
-                <Image
-                  src="/assets/logos/nhs-scotland.svg"
-                  width={153}
-                  height={130}
-                  alt="NHS Scotland logo (opens in a new window)"
-                />
-              </a>
-            </div>
-            <div className="flex items-center">
-              <a href="#" target="_blank">
-                <Image
-                  src="/assets/logos/health-and-care-research-wales.svg"
-                  width={168}
-                  height={106}
-                  alt="Health Care Research Wales logo (opens in a new window)"
-                />
-              </a>
-            </div>
+          <div className="m-auto flex max-w-[320px] flex-wrap items-center justify-center gap-8 py-7 sm:max-w-none md:flex-nowrap lg:justify-between">
+            <a href="#" target="_blank" className="max-w-[240px] lg:w-auto">
+              <Image
+                src="/assets/logos/nihr.svg"
+                width={240}
+                height={24}
+                alt="National Institute for Health and Care Research logo (opens in a new window)"
+                className="mx-auto"
+              />
+            </a>
+            <a href="#" target="_blank" className="max-w-[240px] lg:w-auto">
+              <Image
+                src="/assets/logos/hsc-public-health-agency.svg"
+                width={253}
+                height={97}
+                alt="HSC Public Health Agency logo (opens in a new window)"
+                className="mx-auto"
+              />
+            </a>
+            <a href="#" target="_blank" className="max-w-[240px] lg:w-auto">
+              <Image
+                src="/assets/logos/nhs-scotland.svg"
+                width={153}
+                height={130}
+                alt="NHS Scotland logo (opens in a new window)"
+                className="mx-auto"
+              />
+            </a>
+            <a href="#" target="_blank" className="max-w-[240px] lg:w-auto">
+              <Image
+                src="/assets/logos/health-and-care-research-wales.svg"
+                width={168}
+                height={106}
+                alt="Health Care Research Wales logo (opens in a new window)"
+                className="mx-auto"
+              />
+            </a>
           </div>
         </div>
       </div>
