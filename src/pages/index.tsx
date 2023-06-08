e176c5b2a3e9f7d17f5d75d2104d9f02e0ba8eab
dcnihr <dale.christian@nihr.ac.uk>
2023-06-08 15:47:37 +0100

    fix: styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,18 +31,18 @@ export default function Home({
 
       {/* Title, Description & Video */}
       <Container>
-        <section className="flex flex-wrap items-center gap-4 lg:flex-nowrap lg:pt-6">
+        <section className="flex flex-wrap items-center lg:flex-nowrap lg:gap-4 lg:pt-4">
           <div>
             <h2 className="govuk-heading-l">{title}</h2>
             <p
-              className="whitespace-pre-wrap pr-12"
+              className="whitespace-pre-wrap lg:pr-6"
               dangerouslySetInnerHTML={{
                 __html: description,
               }}
             />
           </div>
           {videoUrl && (
-            <div className="mt-10 flex w-full justify-center">
+            <div className="lg:mt-10 mt-5 flex w-full justify-center">
               <iframe
                 className="aspect-video w-full max-w-[700px] lg:w-[450px]"
                 src={videoUrl}
@@ -57,21 +57,21 @@ export default function Home({
       </Container>
 
       {/* Service Info */}
-      <section className="mt-8 bg-grey-30 pb-4 pt-12 text-center">
+      <section className="mt-7 bg-grey-30 py-7 text-center lg:mt-6">
         <Container>
           <p className="govuk-body-l">
             Each of the data service providers offers one, two or all of the following three services:
           </p>
-          <div className="my-8 grid gap-6 text-left md:grid-cols-3">
+          <div className="my-6 grid gap-6 text-left md:grid-cols-3">
             {/* Find */}
             <Card>
               <div className="flex items-center justify-between bg-[var(--colour-find-background)]">
-                <h3 className="govuk-heading-m mb-0 pl-5 text-navy-100">Find</h3>
-                <div className="bg-[var(--colour-find-foreground)] p-4">
+                <h3 className="govuk-heading-m mb-0 pl-4 text-navy-100">Find</h3>
+                <div className="bg-[var(--colour-find-foreground)] p-3">
                   <FindIcon />
                 </div>
               </div>
-              <div className="flex flex-grow flex-col justify-between p-5">
+              <div className="govuk-body mb-0 flex flex-grow flex-col justify-between p-4">
                 <p>{serviceDescriptionFind}</p>
                 <Link href="/providers?serviceType=Find" className="govuk-link">
                   View all Find services
@@ -81,12 +81,12 @@ export default function Home({
             {/* Recruit */}
             <Card>
               <div className="flex items-center justify-between bg-[var(--colour-recruit-background)]">
-                <h3 className="govuk-heading-m mb-0 pl-5 text-navy-100">Recruit</h3>
-                <div className="bg-[var(--colour-recruit-foreground)] p-4">
+                <h3 className="govuk-heading-m mb-0 pl-4 text-navy-100">Recruit</h3>
+                <div className="bg-[var(--colour-recruit-foreground)] p-3">
                   <RecruitIcon />
                 </div>
               </div>
-              <div className="flex flex-grow flex-col justify-between p-5">
+              <div className="govuk-body mb-0 flex flex-grow flex-col justify-between p-4">
                 <p>{serviceDescriptionRecruit}</p>
                 <Link href="/providers?serviceType=Recruit" className="govuk-link">
                   View all Recruit services
@@ -96,12 +96,12 @@ export default function Home({
             {/* Follow-up */}
             <Card>
               <div className="flex items-center justify-between bg-[var(--colour-follow-up-background)]">
-                <h3 className="govuk-heading-m mb-0 pl-5 text-navy-100">Follow-up</h3>
-                <div className="bg-[var(--colour-follow-up-foreground)] p-4">
+                <h3 className="govuk-heading-m mb-0 pl-4 text-navy-100">Follow-up</h3>
+                <div className="bg-[var(--colour-follow-up-foreground)] p-3">
                   <FollowUpIcon />
                 </div>
               </div>
-              <div className="flex flex-grow flex-col justify-between p-5">
+              <div className="govuk-body mb-0 flex flex-grow flex-col justify-between p-4">
                 <p>{serviceDescriptionFollowUp}</p>
                 <Link href="/providers?serviceType=Follow-up" className="govuk-link">
                   View all Follow-up services
@@ -109,7 +109,7 @@ export default function Home({
               </div>
             </Card>
           </div>
-          <Link className="govuk-button govuk-button--start md:p-4" href="/providers">
+          <Link className="govuk-button govuk-button--start mb-0 md:p-4" href="/providers">
             View all data service providers
             <ChevronIcon className="govuk-button__start-icon" />
           </Link>
@@ -118,31 +118,31 @@ export default function Home({
 
       {/* Sign-posts */}
       <Container>
-        <section className="my-12 text-left">
-          <div className="grid gap-8 md:grid-cols-2">
-            <Card>
-              <div className="p-7 pb-0">
+        <section className="mb-4 mt-7 text-left lg:mt-9">
+          <div className="grid gap-7 md:grid-cols-2 lg:gap-8">
+            <Card className="p-5 lg:p-6">
+              <div className="pb-0">
                 <h3 className="govuk-heading-m heading-underscore mb-0 text-navy-100">Get support for your research</h3>
               </div>
-              <div className="flex flex-grow flex-col items-start justify-between p-7">
+              <div className="flex flex-grow flex-col items-start justify-between pt-4">
                 <p>{signPostDescription1}</p>
                 <div>
-                  <Link className="govuk-button govuk-button--secondary mb-0 text-left" href="#">
+                  <Link className="govuk-button govuk-button--secondary mb-0 mt-2 text-left" href="#">
                     Contact research support
                   </Link>
                 </div>
               </div>
             </Card>
-            <Card>
-              <div className="p-7 pb-0">
+            <Card className="p-5 lg:p-6">
+              <div className="pb-0">
                 <h3 className="govuk-heading-m heading-underscore mb-0 text-navy-100">
                   Become a data service provider
                 </h3>
               </div>
-              <div className="flex flex-grow flex-col items-start justify-between p-7">
+              <div className="flex flex-grow flex-col items-start justify-between pt-4">
                 <p>{signPostDescription2}</p>
                 <div>
-                  <Link className="govuk-button govuk-button--secondary mb-0 text-left" href="#">
+                  <Link className="govuk-button govuk-button--secondary mb-0 mt-2 text-left" href="#">
                     Becoming a data service provider
                   </Link>
                 </div>
