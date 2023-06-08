ef90d17d35ad00c227d6dab1834e3710c14d1af9
Chris McNeill <chris.mcneill@paconsulting.com>
2023-06-08 20:55:38 +0100

    updating smoke test with steps for BDD output

--- qa/tests/features/smokeTests/smoke.e2e.ts
+++ qa/tests/features/smokeTests/smoke.e2e.ts
@@ -1,23 +1,62 @@
 import { test } from '../../../hooks/CustomFixtures'
 
-test('Can Navigate to Home Page', async ({ homePage }) => {
-  await homePage.goto()
-  await homePage.assertOnHomePage()
-})
+test.describe('Basic Smoke Tests for Initial Site Testing', () => {
+  test('As a User I Can Navigate to the Home Page', async ({ homePage }) => {
+    await test.step('Given I have navigated to the HomePage', async () => {
+      await homePage.goto()
+    })
+    await test.step('Then I should see the Home Page', async () => {
+      await homePage.assertOnHomePage()
+    })
+  })
 
-test('Check that Common Site Items Appear', async ({ homePage, commonItemsPage }) => {
-  await homePage.goto()
-  await commonItemsPage.assertBannerItemsAppear()
-})
+  test('As a User I Can Navigate to Providers Page from Home Page', async ({ homePage, providersPage }) => {
+    await test.step('Given I have navigated to the HomePage', async () => {
+      await homePage.goto()
+    })
+    await test.step('When I click the Providers button', async () => {
+      await homePage.btnProviders.click()
+    })
+    await test.step('Then I should see the Providers Page', async () => {
+      await providersPage.assertOnProvidersPage()
+    })
+  })
 
-test('Can Navigate to Providers Page from Home Page', async ({ homePage, providersPage }) => {
-  await homePage.goto()
-  await homePage.btnProviders.click()
-  await providersPage.assertOnProvidersPage()
-})
+  test('As a User I Can Navigate to Provider Details Page from Providers Page', async ({
+    providersPage,
+    providerDetailsPage,
+  }) => {
+    await test.step('Given I have navigated to the Providers Page', async () => {
+      await providersPage.goto()
+    })
+    await test.step('When I click the Detail button', async () => {
+      await providersPage.btnDetail.click()
+    })
+    await test.step('Then I should see the Provider Details Page', async () => {
+      await providerDetailsPage.assertOnProviderDetailsPage()
+    })
+  })
 
-test('Can Navigate to Provider Details Page from Providers Page', async ({ providersPage, providerDetailsPage }) => {
-  await providersPage.goto()
-  await providersPage.btnDetail.click()
-  await providerDetailsPage.assertOnProviderDetailsPage()
+  test('As a User I Can See That a Banner with Shared Items Appears Consistently Across the Site', async ({
+    homePage,
+    providersPage,
+    providerDetailsPage,
+    commonItemsPage,
+  }) => {
+    await test.step('Given I have navigated to the HomePage', async () => {
+      await homePage.goto()
+    })
+    await test.step('Then I should see the Banner Items Appear', async () => {
+      await commonItemsPage.assertBannerItemsAppear()
+    })
+    await test.step('Given I have navigated to the Providers Page', async () => {
+      await providersPage.goto()
+    })
+    await test.step('Then I should see the Banner Items Appear', async () => {
+      await commonItemsPage.assertBannerItemsAppear()
+    })
+    await test.step('Given I have navigated to the Provider Details Page', async () => {
+      await providerDetailsPage.goto()
+    })
+  })
 })
