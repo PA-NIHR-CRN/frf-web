b7d2cd038b3f6348e9fe3012e642f4fef9474331
Chris McNeill <chris.mcneill@paconsulting.com>
2023-06-09 11:50:58 +0100

    adding config to test yml file, to run in dev and test env based on job input

--- qa/hooks/GlobalSetup.ts
+++ qa/hooks/GlobalSetup.ts
@@ -1,7 +1,10 @@
-import { FullConfig } from '@playwright/test'
-
-async function globalSetup(config: FullConfig) {
-  process.env.BASE_URL = 'https://dev.findrecruitandfollowup.nihr.ac.uk/'
+async function globalSetup() {
+  if (process.env.ENV_CONTEXT == 'DEV') {
+    process.env.BASE_URL = 'https://dev.findrecruitandfollowup.nihr.ac.uk/'
+  } else if (process.env.ENV_CONTEXT == 'TEST') {
+    process.env.BASE_URL = 'https://test.findrecruitandfollowup.nihr.ac.uk/'
+  } else {
+    throw Error('NO ENVIRONMENT SPECIFIED - STOPPING TEST EXECUTION')
+  }
 }
-
 export default globalSetup
