50f82819a33979e39012df629610944c46d88519
dcnihr <dale.christian@nihr.ac.uk>
2023-06-16 11:54:09 +0100

    fix: contrast issue

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 export function Tag({ children, ...props }: { children: ReactNode }) {
   return (
     <strong
-      className={clsx('govuk-tag', 'govuk-tag--red ml-3 rounded-xl border-0 bg-coral-100 uppercase text-white')}
+      className={clsx('govuk-tag', 'govuk-tag--red ml-3 rounded-xl border-0 bg-coral-140 uppercase text-white')}
       {...props}
     >
       {children}
