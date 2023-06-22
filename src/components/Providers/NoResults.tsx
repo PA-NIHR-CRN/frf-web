d47296a48c52952c470d2ce39074a91305b0f3f1
dcnihr <dale.christian@nihr.ac.uk>
2023-06-22 20:01:57 +0100

    refactor: label list

--- src/components/Providers/NoResults.tsx
+++ src/components/Providers/NoResults.tsx
@@ -4,8 +4,8 @@ export function NoResults() {
   return (
     <div className="govuk-!-margin-top-8 govuk-body">
       <h3 className="govuk-heading-l">There are no matching results.</h3>
-      <p>Improve your search results by:</p>
-      <List className="pl-6">
+      <p id="improve-search-results">Improve your search results by:</p>
+      <List className="pl-6" aria-labelledby="improve-search-results">
         <ListItem>Removing filters</ListItem>
         <ListItem>Double-checking your spelling</ListItem>
         <ListItem>Using fewer keywords</ListItem>
