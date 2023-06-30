7b7aa18cef6f7be4161dd60c3ec4a84564216ba1
Chris McNeill <chris.mcneill@paconsulting.com>
2023-06-30 08:52:43 +0100

    adding access test for details page, commenting out schedule run for now

--- qa/tests/features/accessibilityTests/axeAccessibilityTests.e2e.ts
+++ qa/tests/features/accessibilityTests/axeAccessibilityTests.e2e.ts
@@ -87,3 +87,30 @@ test.describe('DSP List Page Accessibility Tests - @access_DspListPage', () => {
     })
   })
 })
+
+test.describe('DSP Details Page Accessibility Tests - @access_DspDetailPage', () => {
+  test('Scan DSP Details Page with AXE Tool', async ({ providerDetailsPage, makeAxeBuilder }, testInfo) => {
+    const axeScanner = makeAxeBuilder()
+    let axeScanResults: any
+    await test.step('Given I have navigated to a DSP Details Page', async () => {
+      await providerDetailsPage.goto()
+      await providerDetailsPage.assertOnProviderDetailsPage()
+    })
+
+    await test.step('When I scan the DSP Details Page for Accessibility Errors', async () => {
+      axeScanResults = await axeScanner
+        .options({ reporter: 'v2' })
+        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+        .analyze()
+    })
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(axeScanResults, null, 2),
+      contentType: 'application/json',
+    })
+
+    await test.step('Then I should recieve no issue up to WCAG 2.1 AA Standard', async () => {
+      expect(await axeScanResults.violations).toEqual([])
+    })
+  })
+})
