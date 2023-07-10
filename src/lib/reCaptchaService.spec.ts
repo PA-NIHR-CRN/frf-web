d6f546e95db93f28f8800355c4afc8faaeba1770
tctc91 <git-commit-address>
2023-07-10 08:13:50 +0100

    chore: recaptcha catch block unit test

--- src/lib/reCaptchaService.spec.ts
+++ src/lib/reCaptchaService.spec.ts
@@ -40,3 +40,24 @@ test('Invalid reCaptcha token', async () => {
 
   expect(valid).toBeFalsy()
 })
+
+test('Unexpected error', async () => {
+  console.log = jest.fn()
+
+  server.use(
+    rest.post('https://recaptchaenterprise.googleapis.com/v1/projects/mock-project-id/assessments', (req, res, ctx) =>
+      res(ctx.json(null))
+    )
+  )
+
+  const reCaptchaService = new ReCaptchaService({
+    projectId: 'mock-project-id',
+    siteKey: 'mock-site-key',
+    apiKey: 'mock-api-key',
+  })
+
+  const { valid } = await reCaptchaService.validateToken('mock-token')
+
+  expect(valid).toBeFalsy()
+  expect(console.log).toHaveBeenCalledWith(new Error("Cannot read properties of null (reading 'tokenProperties')"))
+})
