0032d39fa4778475b4029df8b19560f25f85eddc
tcnihr <135021025+tcnihr@users.noreply.github.com>
2023-07-17 14:04:52 +0100

    Fix: GTM in prod (#91)
    * fix: env
    
    * fix: env var for preview
    
    ---------
    
    Co-authored-by: tctc91 <git-commit-address>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,9 +19,7 @@ export default function Document() {
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${process.env.GOOGLE_ANALYTICS_AUTH}&gtm_preview=env-${
-            process.env.APP_ENV === 'prod' ? '1' : '6'
-          }&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${process.env.GOOGLE_ANALYTICS_AUTH}&gtm_preview=${process.env.GOOGLE_ANALYTICS_PREVIEW}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${process.env.GOOGLE_ANALYTICS_ID}');`}
         </Script>
         <Main />
