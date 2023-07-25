507cb81372799a6211c3d57db68f1be6529ea43d
tcnihr <135021025+tcnihr@users.noreply.github.com>
2023-07-25 15:52:34 +0100

    fix: use router back to persist page (#108)
    Co-authored-by: tctc91 <git-commit-address>

--- src/components/Layout/ServiceProviderLayout.tsx
+++ src/components/Layout/ServiceProviderLayout.tsx
@@ -1,17 +1,26 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
 import { Container } from '../Container/Container'
 import { RootLayout } from './RootLayout'
 
 export function ServiceProviderLayout({ children, isPreviewMode }: { children: ReactNode; isPreviewMode?: boolean }) {
+  const router = useRouter()
   return (
     <RootLayout
       isPreviewMode={isPreviewMode}
       backLink={
         !isPreviewMode && (
           <Container>
-            <Link href="/providers" className="govuk-back-link">
+            <Link
+              href="/providers"
+              className="govuk-back-link"
+              onClick={(event) => {
+                event.preventDefault()
+                router.back()
+              }}
+            >
               Back to list of data service providers
             </Link>
           </Container>
