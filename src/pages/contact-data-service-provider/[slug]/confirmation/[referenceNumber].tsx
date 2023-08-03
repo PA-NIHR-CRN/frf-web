93e1606e4dd45061d9486ebaf07a90ea2aab6a71
tctc91 <git-commit-address>
2023-08-03 14:51:39 +0100

    fix: confirmation pages

--- src/pages/contact-data-service-provider/[slug]/confirmation/[referenceNumber].tsx
+++ src/pages/contact-data-service-provider/[slug]/confirmation/[referenceNumber].tsx
@@ -1,5 +1,6 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import Link from 'next/link'
+import { NextSeo } from 'next-seo'
 
 import { Container } from '@/components/Container/Container'
 import { contentfulService } from '@/lib/contentful'
@@ -12,24 +13,27 @@ export default function ContactDataServiceProviderConfirmation({
   referenceNumber,
 }: ContactDataServiceProviderConfirmationProps) {
   return (
-    <Container>
-      <div className="govuk-grid-row">
-        <div className="govuk-grid-column-two-thirds-from-desktop">
-          <h2 className="govuk-heading-l">Thank you</h2>
-          <p>Your enquiry has been sent to {name}.</p>
-          <p>
-            {referenceNumber && `Your enquiry reference number is ${referenceNumber}. `}A copy of your enquiry will be
-            sent to your email address.
-          </p>
-          <p>
-            <Link href="/feedback">What did you think of this website?</Link> (takes 30 seconds)
-          </p>
-          <Link href="/" className="govuk-button" data-module="govuk-button">
-            Return to homepage
-          </Link>
+    <>
+      <NextSeo title={`Thank you for contacting ${name} - Find, Recruit and Follow-up`} />
+      <Container>
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds-from-desktop">
+            <h2 className="govuk-heading-l">Thank you</h2>
+            <p>Your enquiry has been sent to {name}.</p>
+            <p>
+              {referenceNumber && `Your enquiry reference number is ${referenceNumber}. `}A copy of your enquiry will be
+              sent to your email address.
+            </p>
+            <p>
+              <Link href="/feedback">What did you think of this website?</Link> (takes 30 seconds)
+            </p>
+            <Link href="/" className="govuk-button" data-module="govuk-button">
+              Return to homepage
+            </Link>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   )
 }
 
@@ -48,7 +52,7 @@ export const getServerSideProps = async ({ query }: GetServerSidePropsContext) =
 
     return {
       props: {
-        page: `Confirmation - Contact data service provider - ${name}`,
+        page: `Thank you for contacting ${name}`,
         name,
         referenceNumber,
         isPreviewMode: parseInt(process.env.CONTENTFUL_PREVIEW_MODE) === 1,
--- src/pages/contact-frf-team/confirmation/[referenceNumber].tsx
+++ src/pages/contact-frf-team/confirmation/[referenceNumber].tsx
@@ -1,55 +1,45 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import Link from 'next/link'
+import { NextSeo } from 'next-seo'
 
 import { Container } from '@/components/Container/Container'
-import { contentfulService } from '@/lib/contentful'
 import { logger } from '@/lib/logger'
 
-export type ContactDataServiceProviderConfirmationProps = InferGetServerSidePropsType<typeof getServerSideProps>
+export type ContactFrfTeamConfirmationProps = InferGetServerSidePropsType<typeof getServerSideProps>
 
-export default function ContactDataServiceProviderConfirmation({
-  name,
-  referenceNumber,
-}: ContactDataServiceProviderConfirmationProps) {
+export default function ContactFrfTeamConfirmation({ referenceNumber }: ContactFrfTeamConfirmationProps) {
   return (
-    <Container>
-      <div className="govuk-grid-row">
-        <div className="govuk-grid-column-two-thirds-from-desktop">
-          <h2 className="govuk-heading-l">Thank you</h2>
-          <p>Your enquiry has been sent to {name}.</p>
-          <p>
-            {referenceNumber && `Your enquiry reference number is ${referenceNumber}. `}A copy of your enquiry will be
-            sent to your email address.
-          </p>
-          <p>
-            <Link href="/feedback">What did you think of this website?</Link> (takes 30 seconds)
-          </p>
-          <Link href="/" className="govuk-button" data-module="govuk-button">
-            Return to homepage
-          </Link>
+    <>
+      <NextSeo title="Thank you for contacting the Find, Recruit and Follow-up specialist team - Find, Recruit and Follow-up" />
+      <Container>
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds-from-desktop">
+            <h2 className="govuk-heading-l">Thank you</h2>
+            <p>We have received your enquiry and will be in touch in due course.</p>
+            <p>
+              {referenceNumber && `Your enquiry reference number is ${referenceNumber}. `}A copy of your enquiry will be
+              sent to your email address.
+            </p>
+            <p>
+              <Link href="/feedback">What did you think of this website?</Link> (takes 30 seconds)
+            </p>
+            <Link href="/" className="govuk-button" data-module="govuk-button">
+              Return to homepage
+            </Link>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   )
 }
 
 export const getServerSideProps = async ({ query }: GetServerSidePropsContext) => {
-  const slug = String(query.slug)
   const referenceNumber = String(query.referenceNumber)
 
   try {
-    const entry = await contentfulService.getProviderBySlug(slug)
-
-    if (!entry) throw new Error('Failed to fetch provider by slug: null entry')
-
-    const {
-      fields: { name },
-    } = entry
-
     return {
       props: {
-        page: `Confirmation - Contact data service provider - ${name}`,
-        name,
+        page: `Thank you for contacting the Find, Recruit and Follow-up specialist team`,
         referenceNumber,
         isPreviewMode: parseInt(process.env.CONTENTFUL_PREVIEW_MODE) === 1,
       },
