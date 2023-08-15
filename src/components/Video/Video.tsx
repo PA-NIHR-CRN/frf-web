145dbbfc312a9477b79a9e11273158038c7557fc
dcnihr <dale.christian@nihr.ac.uk>
2023-08-16 00:52:57 +0100

    fix: update youtube domain based on cookie

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,3 +1,7 @@
+import { getCookie } from 'cookies-next'
+import { useEffect, useState } from 'react'
+
+import { COOKIE_SETTINGS_CHANGE_EVENT, FRF_GDPR_COOKIE_ACCEPT_VALUE, FRF_GDPR_COOKIE_NAME } from '@/constants/cookies'
 import { getVideoID } from '@/utils'
 
 export type VideoProps = {
@@ -5,9 +9,32 @@ export type VideoProps = {
   title: string
 }
 
+const YOUTUBE_DOMAIN = 'youtube.com'
+const YOUTUBE_NOCOOKIE_DOMAIN = 'www.youtube-nocookie.com'
+
 export const Video = ({ url, title }: VideoProps) => {
+  const [youTubeDomain, setYouTubeDomain] = useState(YOUTUBE_NOCOOKIE_DOMAIN)
+
+  // Set correct domain on first render
+  useEffect(() => {
+    const hasAcceptedCookies = getCookie(FRF_GDPR_COOKIE_NAME) === FRF_GDPR_COOKIE_ACCEPT_VALUE
+    if (hasAcceptedCookies) {
+      setYouTubeDomain(YOUTUBE_DOMAIN)
+    }
+  }, [])
+
+  // Handle user accepting/rejecting cookies
+  useEffect(() => {
+    const handleCookiesChange = (event: CustomEventInit) => {
+      setYouTubeDomain(event.detail === 1 ? YOUTUBE_DOMAIN : YOUTUBE_NOCOOKIE_DOMAIN)
+    }
+    document.addEventListener(COOKIE_SETTINGS_CHANGE_EVENT, handleCookiesChange)
+    return () => document.removeEventListener(COOKIE_SETTINGS_CHANGE_EVENT, handleCookiesChange)
+  }, [])
+
   const videoID = getVideoID(url)
-  const videoURL = `https://www.youtube-nocookie.com/embed/${videoID}`
+  const videoURL = `https://${youTubeDomain}/embed/${videoID}`
+
   return (
     <iframe
       className="aspect-video w-full max-w-[700px] lg:w-[450px]"
