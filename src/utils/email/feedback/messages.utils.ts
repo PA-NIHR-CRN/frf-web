02f781c3fac4cff74b0c5e5d011e9e95815de1f3
tctc91 <git-commit-address>
2023-08-23 16:15:16 +0100

    feat: feedback form hooked up to contentful

--- src/utils/email/feedback/messages.utils.ts
+++ src/utils/email/feedback/messages.utils.ts
@@ -1,18 +1,30 @@
-import { EMAIL_FRF_INBOX } from '@/constants'
-import { EmailArgs } from '@/lib/email/emailService'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
+
+import { TypeEmailTemplateFeedback } from '@/@types/generated'
+import { EmailArgs } from '@/lib/email/emailServiceV2'
 import { FeedbackInputs } from '@/utils/schemas/feedback.schema'
 
 export type MessageData = FeedbackInputs & { referenceNumber: string }
 
-export const getNotificationMessages = (messageData: MessageData) => {
+export const getNotificationMessages = (
+  messageData: MessageData,
+  contentType: TypeEmailTemplateFeedback<undefined, ''>['fields']
+) => {
   const messages: EmailArgs[] = []
 
+  const { referenceNumber } = messageData
+  const { recipients, subject, body, signature, signatureLogo } = contentType
+
   messages.push({
-    to: EMAIL_FRF_INBOX,
-    subject: `${messageData.referenceNumber} FRF - feedback received`,
-    templateName: 'feedback',
+    to: recipients,
+    subject: subject.replace('{{referenceNumber}}', referenceNumber),
+    bodyHtml: documentToHtmlString(body),
+    bodyText: documentToPlainTextString(body),
     templateData: {
       ...messageData,
+      signatureText: documentToHtmlString(signature),
+      signatureLogo,
     },
   })
 
