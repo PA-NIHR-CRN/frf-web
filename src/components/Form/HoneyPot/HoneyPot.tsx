3a1c1e35bb3d57509ac9e6f43a19dcf4819bccb7
tcnihr <135021025+tcnihr@users.noreply.github.com>
2023-08-25 10:03:23 +0100

    feat: support multiple emails in email contacts and dsp (#158)
    Co-authored-by: tctc91 <git-commit-address>

--- src/components/Form/HoneyPot/HoneyPot.tsx
+++ src/components/Form/HoneyPot/HoneyPot.tsx
@@ -1,5 +1,21 @@
 import { forwardRef } from 'react'
 
 export const HoneyPot = forwardRef<HTMLInputElement>(({ ...props }, ref) => {
-  return <input ref={ref} type="text" className="sr-only" defaultValue="" tabIndex={-1} autoComplete="off" {...props} />
+  return (
+    <>
+      <label>
+        <span className="sr-only">Label</span>
+        <input
+          ref={ref}
+          // https://stackoverflow.com/a/30873633
+          type="search"
+          autoComplete="off"
+          className="sr-only"
+          defaultValue=""
+          tabIndex={-1}
+          {...props}
+        />
+      </label>
+    </>
+  )
 })
