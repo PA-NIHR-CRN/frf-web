c16abc0f74f194a20c251099c66e2a73b23348f4
ynasser25 <113994212+ynasser25@users.noreply.github.com>
2023-10-23 13:23:00 +0100

    change ses region to eu-west-2 (#184)

--- src/lib/email/index.ts
+++ src/lib/email/index.ts
@@ -2,7 +2,7 @@ import * as aws from 'aws-sdk'
 
 import { EmailService } from '@/lib/email/emailService'
 
-aws.config.update({ region: 'eu-west-1' })
+aws.config.update({ region: 'eu-west-2' })
 
 const ses = new aws.SES()
 
