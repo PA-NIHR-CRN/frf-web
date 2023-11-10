13597676ec04be5d38f3efb5439509ab0d1f5752
Thomas Smolen <thomas.smolen@paconsulting.com>
2023-11-10 08:09:40 +0000

    fix: removed refresh error on homepage

--- src/components/Video/Video.spec.tsx
+++ src/components/Video/Video.spec.tsx
@@ -30,10 +30,6 @@ test('Includes necessary attributes in the iframe element', () => {
 
   const videoElement = screen.getByTitle(testProps.title)
   expect(videoElement).toHaveAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope;')
-  expect(videoElement).toHaveAttribute(
-    'srcdoc',
-    `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/ABCDEFG?autoplay=1><img src=https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg alt='Test Video'><span>▶</span></a>`
-  )
   expect(videoElement).toHaveAttribute('allowfullscreen')
 })
 
