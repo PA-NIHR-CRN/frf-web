197def4e0aa3471ab03e603a8e5232987b994c4b
Kevin Officer <>
2023-11-16 16:12:21 +0000

    fix: update label

--- src/components/Form/HoneyPot/HoneyPot.tsx
+++ src/components/Form/HoneyPot/HoneyPot.tsx
@@ -8,12 +8,9 @@ export const HoneyPot = forwardRef<HTMLInputElement>(({ ...props }, ref) => {
   return (
     <>
       <div aria-hidden="true">
-        <label
-          htmlFor="potFilter"
-          className="sr-only"
-          aria-label="Please leave this field empty. Providing information in this field will cause your form submission to fail. The intended purpose of this field is to prevent bots from submitting this form."
-        >
-          Work email address
+        <label htmlFor="potFilter" className="sr-only">
+          Work email address. Please leave this field empty. Providing information in this field will cause your form
+          submission to fail. The intended purpose of this field is to prevent bots from submitting this form.
         </label>
         <input
           id="potFilter"
