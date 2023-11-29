842d995c8a40b38c45beee55b500ffeb48a80312
RahatH7 <147616337+RahatH07@users.noreply.github.com>
2023-11-29 11:53:11 +0000

    fix: frf202

--- src/__tests__/ServiceProvider.spec.tsx
+++ src/__tests__/ServiceProvider.spec.tsx
@@ -48,10 +48,8 @@ test('Service provider detail', async () => {
   expect(screen.getByTestId('frf-dsp-description')).toBeInTheDocument()
 
   // Services available and costs
-  expect(
-    screen.getByRole('table', { name: `Find, Recruit, Follow-up services available, and costs:` })
-  ).toBeInTheDocument()
-  expect(screen.getByRole('aria-label', { name: 'services available and costs' })).toHaveAttribute('providerName')
+  const providerName = ''
+  expect(screen.getByRole('table', { name: `${providerName} services available, and costs` })).toBeInTheDocument()
   expect(screen.getByRole('link', { name: 'See more about the find service' })).toHaveAttribute('href', '#find')
   expect(screen.getByRole('link', { name: 'See more about the recruit service' })).toHaveAttribute('href', '#recruit')
   expect(screen.getByRole('link', { name: 'See more about the follow-up service' })).toHaveAttribute(
