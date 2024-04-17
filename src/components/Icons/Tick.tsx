d1f837415ed288c048152199d8b03a31b0a0494d
Thomas Smolen <thomas.smolen@paconsulting.com>
2024-04-17 08:30:52 +0100

    fix: added flexgrow and flexshrink properties to ensure icons wont be cut off or grow into space

--- src/components/Icons/Tick.tsx
+++ src/components/Icons/Tick.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 
 function Tick() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" fill="none" aria-hidden data-testid="frf-icon-tick">
-      <path
-        stroke="#00703C"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M20.053 6L9.038 17l-5.007-5"
-      ></path>
-    </svg>
+    <div style={{ flexShrink: 0, flexGrow: 0 }}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="24"
+        fill="none"
+        aria-hidden
+        data-testid="frf-icon-tick"
+      >
+        <path
+          stroke="#00703C"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M20.053 6L9.038 17l-5.007-5"
+        ></path>
+      </svg>
+    </div>
   )
 }
 
--- src/components/Icons/Users.tsx
+++ src/components/Icons/Users.tsx
@@ -3,7 +3,7 @@ import { useId } from 'react'
 function Users() {
   const id = useId()
   return (
-    <div style={{ flexShrink: 0 }}>
+    <div style={{ flexShrink: 0, flexGrow: 0 }}>
       <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" fill="none" aria-hidden>
         <g stroke="#25282B" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" clipPath={`url(#${id})`}>
           <path d="M17.027 21v-2a4 4 0 00-4-4h-8a4 4 0 00-4 4v2m8-10a4 4 0 100-8 4 4 0 000 8zm14 10v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
