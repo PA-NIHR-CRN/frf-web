d336d4b38b0340c0bd29c48f72dc03515e735d2b
Kevin Officer <>
2024-05-01 14:27:04 +0100

    fix: update tests to get iframe by alt text instead of title now that title is redundant

--- src/components/Video/Video.spec.tsx
+++ src/components/Video/Video.spec.tsx
@@ -20,7 +20,7 @@ const testProps: VideoProps = {
 test('Renders an iframe with the correct src and title', () => {
   render(<Video {...testProps} />)
 
-  const videoElement = screen.getByTitle(testProps.title)
+  const videoElement = screen.getByAltText(testProps.title)
   expect(videoElement).toBeInTheDocument()
   expect(videoElement).toHaveAttribute('src', 'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg')
 })
@@ -49,17 +49,26 @@ test('Updates domain if cookies have been previously accepted', () => {
 
   render(<Video {...testProps} />)
 
-  expect(screen.getByTitle(testProps.title)).toHaveAttribute('src', 'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg')
+  expect(screen.getByAltText(testProps.title)).toHaveAttribute(
+    'src',
+    'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg'
+  )
 })
 
 test('Updates domain after cookies are accepted', async () => {
   render(<Video {...testProps} />)
 
-  expect(screen.getByTitle(testProps.title)).toHaveAttribute('src', 'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg')
+  expect(screen.getByAltText(testProps.title)).toHaveAttribute(
+    'src',
+    'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg'
+  )
 
   fireEvent(document, new CustomEvent(COOKIE_SETTINGS_CHANGE_EVENT, { detail: 1 }))
 
-  expect(screen.getByTitle(testProps.title)).toHaveAttribute('src', 'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg')
+  expect(screen.getByAltText(testProps.title)).toHaveAttribute(
+    'src',
+    'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg'
+  )
 })
 
 test('Updates domain after cookies are rejected', () => {
@@ -68,9 +77,15 @@ test('Updates domain after cookies are rejected', () => {
 
   render(<Video {...testProps} />)
 
-  expect(screen.getByTitle(testProps.title)).toHaveAttribute('src', 'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg')
+  expect(screen.getByAltText(testProps.title)).toHaveAttribute(
+    'src',
+    'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg'
+  )
 
   fireEvent(document, new CustomEvent(COOKIE_SETTINGS_CHANGE_EVENT, { detail: 0 }))
 
-  expect(screen.getByTitle(testProps.title)).toHaveAttribute('src', 'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg')
+  expect(screen.getByAltText(testProps.title)).toHaveAttribute(
+    'src',
+    'https://img.youtube.com/vi/ABCDEFG/hqdefault.jpg'
+  )
 })
